fdf3ab81d5ea83bb77d1201d1ee3f3af451f614f
Emma Creteur <emma_creteur@hotmail.com>
2023-09-04 12:38:18 +0200

    modif profil + condition ajout notes + moyenne rating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id]) || current_user
     @review = Review.new(user: @user)
+    @reviews = @user.reviews
+
+    total_rating = @reviews.sum(&:rating)
+    @average_rating = total_rating.to_f / @reviews.count
   end
 end
