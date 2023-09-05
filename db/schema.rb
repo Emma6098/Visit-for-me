a462410940e5930312bebbbb584042d8f3960ae8
Emma Creteur <emma_creteur@hotmail.com>
2023-09-05 14:55:48 +0200

    end en trop

--- db/schema.rb
+++ db/schema.rb
@@ -111,8 +111,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_102933) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -120,6 +118,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_102933) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
